e8b4df96af7550bc0bb90638713f8ec1946d8ec6
bad-wolf-jm <jihemme@Time-Lord.local>
2018-02-03 16:11:27 -0500

    Semantic UI templates

---
+++ deeplearn/julia/foo.py
@@ -0,0 +1,15 @@
+#import numpy as np
+import time
+#import os
+#import pandas as pd
+import numpy as np
+
+t = time.time()
+#x = np.random.rand(1000000)
+for i in range(1000000):
+    x = np.random.rand()
+print(time.time() - t)
+#np.mean(x, axis=1)
+#np.mean(x)
+
+#print (2)
---
+++ deeplearn/julia/test_compile.jl
@@ -0,0 +1,20 @@
+#module TestComp
+#export main
+using DataFrames
+
+function main()
+    x = rand(100)
+    #mean(x,2)
+    mean(x)
+    y = DataFrames.DataFrame(x=x)
+    show(y)
+end
+
+
+@time for i ∈ 1:10000000
+    u = rand()
+end
+
+@time rand(1000000)
+
+#show(x)
